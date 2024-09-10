b62bba41a4649e57660cb4214d9904e2c23c44b9
MichelSantiago <michelsantiago38@gmail.com>
2024-09-09 22:33:29 -0300

    teste

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import '../models/user.dart';
+
+Map DUMMY_USERS ={
+  '1': const User(
+      id:'1',
+      name:'Maria',
+      email:'maria@alunos.com.br',
+      avatarUrl? 'sua foto aqui'
+  ),
+  '2': const User(
+      id:'2',
+      name:'Rafael',
+      email:'rafael@alunos.com.br',
+      avatarUrl? 'sua foto aqui'
+  ),
+  '3': const User(
+      id:'3',
+      name:'Fernanda',
+      email:'fernanda@alunos.com.br',
+      avatarUrl? 'sua foto aqui'
+  ),
+  '4': const User(
+      id:'4',
+      name:'Guilherme',
+      email:'Guilherme@alunos.com.br',
+      avatarUrl? 'sua foto aqui'
+  ),
+};
