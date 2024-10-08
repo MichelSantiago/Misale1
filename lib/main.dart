5a3c5b161a85bd6902763326fa94e712f8d293db
MichelSantiago <michelsantiago38@gmail.com>
2024-10-08 09:26:31 -0300

    Foi o que deu!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Misale Ferramentas',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
